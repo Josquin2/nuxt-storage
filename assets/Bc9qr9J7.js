915342b5d2d4108d064827574160393871abc736
Josquin2 <Josquinwork@mail.ru>
2024-09-16 00:01:15 +0300

    Updates

--- assets/Bc9qr9J7.js
+++
@@ -1,1 +0,0 @@
-import{d as e,u as o}from"./DE6_ZdmV.js";const n=e({__name:"index",setup(r){return o().push("/StoragePage"),()=>{}}});export{n as default};
---
+++ assets/T1q-Fx96.js
@@ -0,0 +1,1 @@
+import{d as e,u as o}from"./DA0GBero.js";const n=e({__name:"index",setup(r){return o().push("/StoragePage"),()=>{}}});export{n as default};
