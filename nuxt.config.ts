1071f88e5291daa6aa9ef712493de7d576372b70
Josquin2 <Josquinwork@mail.ru>
2024-09-15 18:00:57 +0300

    fixed for deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/nuxt-storage/",
+    buildAssetsDir: "assets",
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@pinia/nuxt"],
